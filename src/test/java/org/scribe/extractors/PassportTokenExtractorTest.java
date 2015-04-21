5a8bbe21f2377ce1f94073b28e6e0af4811c7ebb
John Alade <john.alade@interswitchng.com>
2015-04-22 00:24:48 +0100

    Passport Test Updated

--- src/test/java/org/scribe/extractors/PassportTokenExtractorTest.java
+++ src/test/java/org/scribe/extractors/PassportTokenExtractorTest.java
@@ -25,45 +25,5 @@ public class PassportTokenExtractorTest
     assertEquals("eyJhdWQiOlsicGFzc3BvcnQiXSwiZXhwIjoxNDI2NjM4NzA0LCJ1c2VyX25hbWUiOiJqb2huLmFsYWRlIiwianRpIjoiNTdjNjNlNGYtY2Q3Zi00OTExLWFiZTAtN2JiMTIyZWJhMWIwIiwiY2xpZW50X2lkIjoic21hcnRpbnN1cmUiLCJzY29wZSI6WyJwcm9maWxlIl19", extracted.getToken());
     assertEquals("", extracted.getSecret());
   }
-/*
-  @Test
-  public void shouldExtractTokenFromResponseWithExpiresParam()
-  {
-    String response = "access_token=166942940015970|2.2ltzWXYNDjCtg5ZDVVJJeg__.3600.1295816400-548517159|RsXNdKrpxg8L6QNLWcs2TVTmcaE&expires=5108";
-    Token extracted = extractor.extract(response);
-    assertEquals("166942940015970|2.2ltzWXYNDjCtg5ZDVVJJeg__.3600.1295816400-548517159|RsXNdKrpxg8L6QNLWcs2TVTmcaE", extracted.getToken());
-    assertEquals("", extracted.getSecret());
-  }
-
-  @Test
-  public void shouldExtractTokenFromResponseWithManyParameters()
-  {
-    String response = "access_token=foo1234&other_stuff=yeah_we_have_this_too&number=42";
-    Token extracted = extractor.extract(response);
-    assertEquals("foo1234", extracted.getToken());
-    assertEquals("", extracted.getSecret());
-  }
-
-  @Test(expected = OAuthException.class)
-  public void shouldThrowExceptionIfTokenIsAbsent()
-  {
-    String response = "&expires=5108";
-    extractor.extract(response);
-  }
-
-  @Test(expected = IllegalArgumentException.class)
-  public void shouldThrowExceptionIfResponseIsNull()
-  {
-    String response = null;
-    extractor.extract(response);
-  }
-
-  @Test(expected = IllegalArgumentException.class)
-  public void shouldThrowExceptionIfResponseIsEmptyString()
-  {
-    String response = "";
-    extractor.extract(response);
-  }
-
-  */
+ 
 }
