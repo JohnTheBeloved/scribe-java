85b30b62c6176d85ce293756d5158c891eda0647
John Alade <john.alade@interswitchng.com>
2015-04-22 00:22:32 +0100

    Accomodated for Passport and Other Format Regex for response

--- src/main/java/org/scribe/extractors/TokenExtractor20Impl.java
+++ src/main/java/org/scribe/extractors/TokenExtractor20Impl.java
@@ -20,17 +20,27 @@ public class TokenExtractor20Impl implements AccessTokenExtractor
    */
   public Token extract(String response)
   {
+    //Please note that I hacked this function to allow decoding of response from passport(interswitchng Passport Provider)
+    //The default mather should be used first(important cos the regex for passport matches default so default matching should come first), 
+    //then if it is incorrect, Passport match is used, after which it can then throw exception
     Preconditions.checkEmptyString(response, "Response body is incorrect. Can't extract a token from an empty string");
 
-    Matcher matcher = Pattern.compile(PASSPORT_TOKEN_REGEX).matcher(response);
-    if (matcher.find())
+    Matcher passportMatcher = Pattern.compile(PASSPORT_TOKEN_REGEX).matcher(response);
+    Matcher defaultMatcher = Pattern.compile(TOKEN_REGEX).matcher(response);
+    
+    if(defaultMatcher.find()){
+      System.out.println("Default..........");
+      String userToken = OAuthEncoder.decode(defaultMatcher.group(1));
+      return new Token(userToken, EMPTY_SECRET, response);
+    }
+    else if (passportMatcher.find())
     {
-      String token = matcher.group(0).replaceAll("access_token\":", "");
+      System.out.println("Passport..........");
+      String token = passportMatcher.group(0).replaceAll("access_token\":", "");
       String userToken = token.split("\\.")[1];
       return new Token(userToken, EMPTY_SECRET, response);
     } 
-    else
-    {
+    else{
       throw new OAuthException("Response body is incorrect. Can't extract a token from this: '" + response + "'", null);
     }
   }
